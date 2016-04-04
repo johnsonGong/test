d1b294d587699e544f1140e8d202f30b7d19aca6
johnsonGong <Administrator@PC-201407051608>
2016-04-04 22:24:33 +0800

    loopDom

---
+++ testLocal/js/loopDom.js
@@ -0,0 +1,32 @@
+/**
+ * 循环遍历 html-dom.
+ *
+ * @author gonglong-201604.
+ *
+ **/
+$(function() {
+
+    console.log("loopDom -->123");
+
+    var target = $("#div01");
+
+    loopDom(target);
+
+    function loopDom(jqObj) {
+
+        jqObj.each(function(idx, item) {
+//          var childs = $(this).children();
+//
+//          if(childs.length ) {
+//
+//          }
+
+            if($(this).children().length === 0) {
+                console.log("id: " + $(this).attr("id") + ", text: " + $(this).text());
+            } else {
+                loopDom($(this).children());
+            }
+
+        });
+    }
+});
